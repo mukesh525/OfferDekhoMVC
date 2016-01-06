4afadd753ff27eba81165beb17170e2fb5f4feb0
mukesh <mukesh@mukesh-PC>
2016-01-06 22:35:35 +0530

    10 commit

--- application/models/products.php
+++ application/models/products.php
@@ -3,11 +3,16 @@
 Class Products extends CI_Model {
     
     public function __construct() {
-         $this->load->database();
+          $this->load->database();
     }
     
     public function getCategory() {
            $query= $this->db->get('category');   
+          // echo $query->num_rows();
+           return $query->result_array();
+    }
+    public function getBrands() {
+           $query= $this->db->get('brand');   
           // echo $query->num_rows();
            return $query->result_array();
     }
