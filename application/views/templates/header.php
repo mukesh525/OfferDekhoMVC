d60d2d60ec6e49ffdeb48e930504c8f6b202a8f7
Mukesh Jha <mukeshjha2007@gmail.com>
2016-01-06 18:54:00 +0530

    7 commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -37,13 +37,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <div class="header-top">
 	 <div class="header-bottom">			
 				<div class="logo">
-					<h1><a href="<?=base_url()?>">Offer Dekho</a></h1>					
+					<h1><a href="">Offer Dekho</a></h1>					
 				</div>
 			 <!---->		 
 			 <div class="top-nav">
 				<ul class="memenu skyblue"><li class="showhide" style="overflow: hidden; display: none;">
                                         <span class="title">MENU</span><span class="icon1"></span>
-                                        <span class="icon2"></span></li><li class="active"><a href="<?=base_url()?>">Home</a></li>
+                                        <span class="icon2"></span></li><li class="active"><a href="index.php">Home</a></li>
 					<li class="grid"><a href="#">Products</a>
 						<div class="mepanel">
 							<div class="row">
@@ -51,7 +51,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 									<h4>Type</h4>
 									<ul>
                                                                           <?php foreach($data as $key => $value) {?>
-                                                                            <li><a href="index.php/welcome/getdetail/<?php echo $value?>"><?php echo $value;?></a></li>
+                                                                            <li><a href="<?=base_url()?>/welcome/getdetail/<?php echo $value?>"><?php echo $value;?></a></li>
                                                                               <?php }?>
 									</ul>
                                                                         
