86efa76c058b26d41ba431666b091b9ac53f5025
mukesh <mukesh@mukesh-PC>
2016-01-08 23:31:43 +0530

    login signout module fixed

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -10,19 +10,23 @@
 <body>
     
   <div class="top">
-   <div>
-<a href="<?php echo site_url('admin/logout');?>" style="float: right;">logout</a>
-</div>
+      <center> <div class="logo2">
+              <h1><a  href="<?=base_url()?>">Offer Dekho</a></h1></div></center>
+ 
 <div>
-<?php echo "Welcome ".$this->session->userdata('username');?>
+
 </div>
 
 </div>
 <div class="left">
     LEFT
+    <div>
+<a href="<?php echo site_url('admin/logout');?>" style="float: right;">logout</a>
+</div>
 </div>
 <div class="main">
     MAIN
+     <?php echo "Welcome ".$this->session->userdata('username');?>
 </div>  
 
 </body>
