39822d6dfa488e90eca24c8011f6003a0db563e9
Mukesh Jha <mukeshjha2007@gmail.com>
2016-01-08 18:09:22 +0530

    11 commit

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,25 +1,26 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
 class Admin extends CI_Controller {
-    
-public function __construct(){
-parent::__construct();
- $this->load->library('form_validation');
- $this->load->model('user_model');
+public function __construct()
+{ 
+   parent::__construct();
+$this->load->library('form_validation');
+$this->load->model('user_model');
+$this->load->library('session');
 }
+public function index()
+{
+   $this->load->helper('url');
+   if(($this->session->userdata('user_id')!="")){
+      redirect(base_url().'admin');
 
-public function index(){
-    
-  
-if(($this->session->userdata('user_id')!="")){
-redirect(site_url('admin/home'));
-}
-else{
-  //  echo 'Index function get called';
+     echo $this->session->userdata('user_id');  
+
+   }
+  else{
    $this->load->view("admin/register_view");
+ }
 }
-}
-
-
 public function login(){
 $rules = array(array('field'=>'l_email','label'=>'Email','rules'=>'required|valid_email'),
 array('field'=>'l_pass','label'=>'Password','rules'=>'required'));
@@ -27,23 +28,25 @@ $this->form_validation->set_rules($rules);
 if($this->form_validation->run() == FALSE){
 $this->index();
 }
-else{
+else
+{
 $auth=$this->user_model->login($this->input->post('l_email'),$this->input->post('l_pass'));
-if($auth){
-redirect(site_url('admin/home'));
-}else{
+if($auth)
+{
+ $this->load->view("admin/home");
+//redirect(site_url('home'));
+}
+else
+{
 $this->index();
- }
 }
 }
-
-public function register(){
+}public function register()
+{
 $this->load->view('admin/register_view');//loads the register_view.php file in views folder
 }
-
-
-
-public function do_register(){
+public function do_register()
+{
 $rules = array(
 array('field'=>'username','label'=>'User Name','rules'=>'trim|required|min_length[4]|max_length[12]'),
 array('field'=>'email','label'=>'Email','rules'=>'trim|required|valid_email|is_unique[users.email]'),
@@ -51,16 +54,18 @@ array('field'=>'password','label'=>'Password','rules'=>'trim|required|min_length
 array('field'=>'gender','label'=>'Gender','rules'=>'required')
 );
 $this->form_validation->set_rules($rules);
-if($this->form_validation->run() == FALSE){
-$this->load->view('register_view');
+if($this->form_validation->run() == FALSE)
+{
+$this->load->view('admin/register_view');
 }
-else{
+else
+{
 $this->user_model->register_user();
-$this->load->view('success');
- }
+ $this->load->view('success');
 }
-
-public function logout(){
+}
+public function logout()
+{
 $this->session->sess_destroy();
 redirect(site_url());
 }
