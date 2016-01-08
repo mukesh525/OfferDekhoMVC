cb5e8573f08e6b2c8ac61faa3e53242a5e52e4c3
Mukesh Jha <mukeshjha2007@gmail.com>
2016-01-08 18:49:54 +0530

    11 commit

--- brand.sql
+++ brand.sql
@@ -1,23 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 06, 2016 at 05:42 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `offerdekho`
+-- Database: `OfferDekho`
 --
 
 -- --------------------------------------------------------
@@ -28,21 +10,94 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `brand` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(25) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `Name` varchar(250) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name` (`Name`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `brand`
 --
 
 INSERT INTO `brand` (`Id`, `Name`) VALUES
-(1, 'Rebook'),
-(2, 'Levis'),
-(3, 'Puma'),
 (4, 'Lee'),
-(5, 'Nike');
+(2, 'Levis'),
+(5, 'Nike'),
+(6, 'Philips'),
+(3, 'Puma'),
+(1, 'Rebook');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(250) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_2` (`Id`),
+  KEY `Name` (`Name`),
+  KEY `Id` (`Id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`Id`, `Name`) VALUES
+(8, 'Bags'),
+(2, 'Clothes'),
+(5, 'Cosmetics'),
+(10, 'Electronics'),
+(3, 'Food'),
+(4, 'Furniture'),
+(1, 'Kitchen'),
+(6, 'Sports'),
+(9, 'Watches');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('c811d45cc9254b051522171e7f1dbe8b', '127.0.0.1', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36', 1452258812, 'a:3:{s:7:"user_id";s:1:"8";s:8:"username";s:5:"root1";s:5:"email";s:15:"root1@gmail.com";}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `discount` int(100) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `location` varchar(500) NOT NULL,
+  `category` int(250) NOT NULL,
+  `brand` int(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `products`
+--
