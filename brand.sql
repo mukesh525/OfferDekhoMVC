1493945743d384f2cb7093a1fd03607c15939546
mukesh <mukesh@mukesh-PC>
2016-01-10 22:15:10 +0530

    Category Brand Module added

--- brand.sql
+++ brand.sql
@@ -1,10 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2016 at 05:43 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `offerdekho`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
 CREATE TABLE IF NOT EXISTS `brand` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(250) NOT NULL,
+  `Name` varchar(25) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `Name` (`Name`),
-  UNIQUE KEY `Id` (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  UNIQUE KEY `Name` (`Name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
 
 --
 -- Dumping data for table `brand`
@@ -13,10 +40,12 @@ CREATE TABLE IF NOT EXISTS `brand` (
 INSERT INTO `brand` (`Id`, `Name`) VALUES
 (4, 'Lee'),
 (2, 'Levis'),
+(9, 'luci'),
 (5, 'Nike'),
-(6, 'Philips'),
 (3, 'Puma'),
-(1, 'Rebook');
+(1, 'Rebook'),
+(22, 'rtr'),
+(41, 'tuio');
 
 -- --------------------------------------------------------
 
@@ -26,12 +55,10 @@ INSERT INTO `brand` (`Id`, `Name`) VALUES
 
 CREATE TABLE IF NOT EXISTS `category` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(250) NOT NULL,
+  `Name` varchar(255) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `Id_2` (`Id`),
-  KEY `Name` (`Name`),
-  KEY `Id` (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  UNIQUE KEY `Name` (`Name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=90 ;
 
 --
 -- Dumping data for table `category`
@@ -39,11 +66,12 @@ CREATE TABLE IF NOT EXISTS `category` (
 
 INSERT INTO `category` (`Id`, `Name`) VALUES
 (8, 'Bags'),
+(68, 'brand'),
 (2, 'Clothes'),
-(5, 'Cosmetics'),
 (10, 'Electronics'),
 (3, 'Food'),
 (4, 'Furniture'),
+(88, 'ggf'),
 (1, 'Kitchen'),
 (6, 'Sports'),
 (9, 'Watches');
@@ -69,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('c811d45cc9254b051522171e7f1dbe8b', '127.0.0.1', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36', 1452258812, 'a:3:{s:7:"user_id";s:1:"8";s:8:"username";s:5:"root1";s:5:"email";s:15:"root1@gmail.com";}');
+('a10cf4fdbb52948b34ef0c7c18142d44', '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36', 1452444055, 'a:4:{s:9:"user_data";s:0:"";s:7:"user_id";s:2:"18";s:8:"username";s:6:"soma12";s:5:"email";s:16:"soma12@gmail.com";}');
 
 -- --------------------------------------------------------
 
@@ -85,7 +113,11 @@ CREATE TABLE IF NOT EXISTS `products` (
   `location` varchar(500) NOT NULL,
   `category` int(250) NOT NULL,
   `brand` int(250) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `category` (`category`),
+  KEY `brand` (`brand`),
+  KEY `category_2` (`category`),
+  KEY `brand_2` (`brand`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
@@ -118,18 +150,36 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
   `gender` varchar(8) NOT NULL,
-  `registered` varchar(16) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `gender`, `registered`) VALUES
-(6, 'vbngn', 'root@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'male', '1452253436'),
-(7, 'root', 'mukesh@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'male', '1452253750'),
-(8, 'root1', 'root1@gmail.com', 'f94c017857ac124ddc8cdf22f7930652', 'male', '1452255397');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `gender`) VALUES
+(9, 'root', 'root@gmail.com', 'f94c017857ac124ddc8cdf22f7930652', 'male'),
+(10, 'Mukesh', 'mukeshjha2007@gmail.com', 'f94c017857ac124ddc8cdf22f7930652', 'female'),
+(11, 'hhgf', 'dsdfsf@gmail.com', '202cb962ac59075b964b07152d234b70', 'male'),
+(12, 'hhgsf', 'dsdfsf@gmail.com', 'c20ad4d76fe97759aa27a0c99bff6710', 'male'),
+(13, 'hhgsf', 'dsdfsf@gmail.com', 'c20ad4d76fe97759aa27a0c99bff6710', 'male'),
+(14, 'hhgsf', 'dsdfsf@gmail.com', 'c20ad4d76fe97759aa27a0c99bff6710', 'male'),
+(15, 'Mukesh', 'muki@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'female'),
+(16, 'Mukeshg', 'muki1@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'male'),
+(17, 'Mukeshww', 'muki1@gmail.com', '6c14da109e294d1e8155be8aa4b1ce8e', 'male'),
+(18, 'soma12', 'soma12@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'female'),
+(19, 'soma12', 'soma12@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'male');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_categoty` FOREIGN KEY (`category`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `fk_type` FOREIGN KEY (`brand`) REFERENCES `products` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
