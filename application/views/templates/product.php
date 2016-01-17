387a3ac66bd6a22b10838a894df348791d0f9412
mukesh <mukesh@mukesh-PC>
2016-01-17 12:36:32 +0530

    Category Brand Module added

--- application/views/templates/product.php
+++ application/views/templates/product.php
@@ -1,7 +1,6 @@
 
 
 
-
 <!--header//-->
 <div class="product-model">	 
 	 <div class="container">
@@ -13,14 +12,19 @@
                       
                
 		 <div class="col-md-11 product-model-sec">
-                       <?php foreach($selections as $key => $row) {?>    
-					<a href="single.php"><div class="product-grid">
+                       <?php foreach($selections as $key => $row){
+                         session_start();
+                       $_SESSION['product'] =$row;?>   
+                      <?php $attributes = array("name" => "imagesliderform");
+                       echo form_open_multipart('admin/view');?>
+                           <a href="#"/><div class="product-grid">
 						<div class="more-product"><span> </span></div>						
 						<div class="product-img b-link-stripe b-animate-go  thickbox">
 							<img src="<?=base_url()?>images/<?php echo $row['image'] ?>" class="img-responsive" alt="">
 							<div class="b-wrapper">
-							<h4 class="b-animate b-from-left  b-delay03">							
-							<button><span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>Quick View</button>
+							<h4 class="b-animate b-from-left  b-delay03">	
+                                                             <input type="hidden" name="image" value="<?php echo $row['image'] ?>"> 
+                                                            <button type="submit"><span class="glyphicon glyphicon-zoom-in" aria-hidden="true"/>Quick View</button>
 							</h4>
 							</div>
 						</div></a>						
@@ -32,14 +36,12 @@
 								  <p class="pric1"><del><?php echo $row['location'] ?></del></p>
 						          <p class="disc">[<?php echo $row['discount'] ?>% Off]</p>
 								</div>
-								<input type="text" class="item_quantity" value="1" />
-								<input type="button" class="item_add items" value="ADD">
 								<div class="clearfix"> </div>
 							</div>												
 							
 						</div>
 					</div>
-               
+                         <?php echo form_close(); ?>
 				<?php } ?>	
 			</div>
                            
